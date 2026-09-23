33647da3b8018143d3ff32ac4dfad0cf564a1192
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:37 -0400

    feat: implement gcd_48_60.f90

---
+++ numerical/gcd_48_60.f90
@@ -0,0 +1,18 @@
+program gcd_48_60
+  implicit none
+  if (gcdv(48_8, 60_8) /= 12_8) then
+    print *, "FAIL gcd_48_60"
+    stop 1
+  end if
+  print *, "PASS gcd_48_60"
+contains
+  integer(8) function gcdv(a, b)
+    integer(8), intent(in) :: a, b
+    integer(8) :: x, y, t
+    x = a; y = b
+    do while (y /= 0)
+      t = mod(x, y); x = y; y = t
+    end do
+    gcdv = x
+  end function gcdv
+end program gcd_48_60
