3cc5675dcad084eb3e59f83e99f09a9290de61a3
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:36 -0400

    feat: implement catalan_7.f90

---
+++ numerical/catalan_7.f90
@@ -0,0 +1,19 @@
+program catalan_7
+  implicit none
+  if (catalan(7) /= 429_8) then
+    print *, "FAIL catalan_7"
+    stop 1
+  end if
+  print *, "PASS catalan_7"
+contains
+  integer(8) function catalan(n)
+    integer, intent(in) :: n
+    integer(8) :: c
+    integer :: k
+    c = 1_8
+    do k = 1, n
+      c = c * 2_8 * int(2 * k - 1, 8) / int(k + 1, 8)
+    end do
+    catalan = c
+  end function catalan
+end program catalan_7
