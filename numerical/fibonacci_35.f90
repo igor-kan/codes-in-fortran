416feb1319fbe4209f48a0fbd659bff905e30efc
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:37 -0400

    feat: implement fibonacci_35.f90

---
+++ numerical/fibonacci_35.f90
@@ -0,0 +1,19 @@
+program fibonacci_35
+  implicit none
+  if (fib(35) /= 9227465_8) then
+    print *, "FAIL fibonacci_35"
+    stop 1
+  end if
+  print *, "PASS fibonacci_35"
+contains
+  integer(8) function fib(n)
+    integer, intent(in) :: n
+    integer(8) :: a, b, t
+    integer :: i
+    a = 0_8; b = 1_8
+    do i = 1, n
+      t = a + b; a = b; b = t
+    end do
+    fib = a
+  end function fib
+end program fibonacci_35
