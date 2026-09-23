15c20a06829d4535799653933a95c61edf2fb5dd
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:36 -0400

    feat: implement fibonacci_2.f90

---
+++ numerical/fibonacci_2.f90
@@ -0,0 +1,19 @@
+program fibonacci_2
+  implicit none
+  if (fib(2) /= 1_8) then
+    print *, "FAIL fibonacci_2"
+    stop 1
+  end if
+  print *, "PASS fibonacci_2"
+contains
+  integer(8) function fib(n)
+    integer, intent(in) :: n
+    integer(8) :: a, b, t
+    integer :: i
+    a = 0_8; b = 1_8
+    do i = 1, n
+      t = a + b; a = b; b = t
+    end do
+    fib = a
+  end function fib
+end program fibonacci_2
