d1cfafd3f9c94f6948a76b05c23d07c6239d0535
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:37 -0400

    feat: implement reverse_int_98765.f90

---
+++ numerical/reverse_int_98765.f90
@@ -0,0 +1,18 @@
+program reverse_int_98765
+  implicit none
+  if (revint(98765_8) /= 56789_8) then
+    print *, "FAIL reverse_int_98765"
+    stop 1
+  end if
+  print *, "PASS reverse_int_98765"
+contains
+  integer(8) function revint(n)
+    integer(8), intent(in) :: n
+    integer(8) :: m, r
+    m = n; r = 0_8
+    do while (m > 0)
+      r = r * 10_8 + mod(m, 10_8); m = m / 10_8
+    end do
+    revint = r
+  end function revint
+end program reverse_int_98765
