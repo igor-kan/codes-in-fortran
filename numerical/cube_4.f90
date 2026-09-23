b8769d23f7642ba791a99d0e28ff15a5cb398dba
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:36 -0400

    feat: implement cube_4.f90

---
+++ numerical/cube_4.f90
@@ -0,0 +1,13 @@
+program cube_4
+  implicit none
+  if (cube(4) /= 64_8) then
+    print *, "FAIL cube_4"
+    stop 1
+  end if
+  print *, "PASS cube_4"
+contains
+  integer(8) function cube(n)
+    integer, intent(in) :: n
+    cube = int(n, 8) * int(n, 8) * int(n, 8)
+  end function cube
+end program cube_4
