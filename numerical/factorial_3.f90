c6be776ad9990f2e21188757e4b26a34edd25a05
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:36 -0400

    feat: implement factorial_3.f90

---
+++ numerical/factorial_3.f90
@@ -0,0 +1,17 @@
+program factorial_3
+  implicit none
+  if (fact(3) /= 6_8) then
+    print *, "FAIL factorial_3"
+    stop 1
+  end if
+  print *, "PASS factorial_3"
+contains
+  integer(8) function fact(n)
+    integer, intent(in) :: n
+    integer :: i
+    fact = 1_8
+    do i = 2, n
+      fact = fact * i
+    end do
+  end function fact
+end program factorial_3
