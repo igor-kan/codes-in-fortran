098a5ed22b1f3f54ff97ea72c89a98e02db7d272
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:37 -0400

    feat: implement power_11_2.f90

---
+++ numerical/power_11_2.f90
@@ -0,0 +1,17 @@
+program power_11_2
+  implicit none
+  if (powv(11, 2) /= 121_8) then
+    print *, "FAIL power_11_2"
+    stop 1
+  end if
+  print *, "PASS power_11_2"
+contains
+  integer(8) function powv(a, b)
+    integer, intent(in) :: a, b
+    integer :: i
+    powv = 1_8
+    do i = 1, b
+      powv = powv * a
+    end do
+  end function powv
+end program power_11_2
